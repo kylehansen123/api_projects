af75ac8707a5e88a63d74b51da5d6b8d0735111a
Kyle Hansen <kylehansen123@gmail.com>
2014-10-27 03:03:57 -0500

    Kyle Hansen API Homework

--- address_to_weather.rb
+++ address_to_weather.rb
@@ -4,8 +4,8 @@ require 'json'
 # If you experience an error relating to SSL,
 #   uncomment the following two lines:
 
-# require 'openssl'
-# OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+require 'openssl'
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
 puts "Let's get the weather forecast for your address."
 
@@ -15,8 +15,23 @@ url_safe_address = URI.encode(the_address)
 
 # Your code goes below.
 
+url_of_geo_data = "http://maps.googleapis.com/maps/api/geocode/json?address=" + url_safe_address
+raw_geo_data = open(url_of_geo_data).read
+parsed_geo_data = JSON.parse(raw_geo_data)
+
+the_latitude = parsed_geo_data["results"][0]["geometry"]["location"]["lat"]
+the_longitude = parsed_geo_data["results"][0]["geometry"]["location"]["lng"]
+
+url_of_weather_data = "https://api.forecast.io/forecast/cde2501b2b09bfe2cff0e52364691e70/" + the_latitude.to_s + "," + the_longitude.to_s
+raw_weather_data = open(url_of_weather_data).read
+parsed_weather_data = JSON.parse(raw_weather_data)
+
+the_temperature = parsed_weather_data["currently"]["temperature"]
+the_hour_outlook = parsed_weather_data["hourly"]["summary"]
+the_day_outlook = parsed_weather_data["daily"]["summary"]
+
 # Ultimately, we want the following line to work when uncommented:
 
-# puts "The current temperature at #{the_address} is #{the_temperature} degrees."
-# puts "The outlook for the next hour is: #{the_hour_outlook}"
-# puts "The outlook for the next day is: #{the_day_outlook}"
+puts "The current temperature at #{the_address} is #{the_temperature} degrees."
+puts "The outlook for the next hour is: #{the_hour_outlook}"
+puts "The outlook for the next day is: #{the_day_outlook}"
--- coords_to_weather.rb
+++ coords_to_weather.rb
@@ -4,8 +4,8 @@ require 'json'
 # If you experience an error relating to SSL,
 #   uncomment the following two lines:
 
-# require 'openssl'
-# OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+require 'openssl'
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
 puts "Let's get the weather forecast for your location."
 
@@ -19,8 +19,16 @@ the_longitude = gets.chomp
 #   address_to_coords.rb to read from a remote API and parse
 #   the results.
 
+url_of_weather_data = "https://api.forecast.io/forecast/cde2501b2b09bfe2cff0e52364691e70/" + the_latitude.to_s + "," + the_longitude.to_s
+raw_weather_data = open(url_of_weather_data).read
+parsed_weather_data = JSON.parse(raw_weather_data)
+
+the_temperature = parsed_weather_data["currently"]["temperature"]
+the_hour_outlook = parsed_weather_data["hourly"]["summary"]
+the_day_outlook = parsed_weather_data["daily"]["summary"]
+
 # Ultimately, we want the following line to work when uncommented:
 
-# puts "The current temperature at #{the_latitude}, #{the_longitude} is #{the_temperature} degrees."
-# puts "The outlook for the next hour is: #{the_hour_outlook}"
-# puts "The outlook for the next day is: #{the_day_outlook}"
+puts "The current temperature at #{the_latitude}, #{the_longitude} is #{the_temperature} degrees."
+puts "The outlook for the next hour is: #{the_hour_outlook}"
+puts "The outlook for the next day is: #{the_day_outlook}"
